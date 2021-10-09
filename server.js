aab8b08b2107879314f46eae5b274e54f814dad3
Gabriel Goller <kaffarell>
2021-10-09 18:06:19 +0200

    :heavy_minus_sign: Removed body-parser as it is deprecated and now integrated in express

--- server.js
+++ server.js
@@ -1,16 +1,15 @@
 const express = require('express');
-var bodyParser = require('body-parser');
-var fs = require('fs');
-var https = require('https');
+const fs = require('fs');
+const https = require('https');
 
-var privateKey = fs.readFileSync( 'ssl/example.com+5-key.pem');
-var certificate = fs.readFileSync( 'ssl/example.com+5.pem' );
+let privateKey = fs.readFileSync( 'ssl/example.com+5-key.pem');
+let certificate = fs.readFileSync( 'ssl/example.com+5.pem' );
 
 
 const app = express();
-var jsonParser = bodyParser.json();
+app.use(express.json());
 
-var data = {
+let data = {
 	alpha: 0,
 	beta: 0,
 	gamma: 0
@@ -21,17 +20,17 @@ let portNumber = process.env.PORT || 3000;
 
 app.use(express.static(__dirname + '/public'));
 
-var options = {
+let options = {
 	key: privateKey,
 	cert: certificate
 }
 
-var server = https.createServer(options, app).listen(portNumber, function(){
+https.createServer(options, app).listen(portNumber, function(){
   console.log("Express server listening on port " + portNumber);
 });
 
 
-app.post('/send', jsonParser, (req, res) => {
+app.post('/send', (req, res) => {
 	data.alpha = req.body.alpha	
 	data.beta = req.body.beta;
 	data.gamma = req.body.gamma;
